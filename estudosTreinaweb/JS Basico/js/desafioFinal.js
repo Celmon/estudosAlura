798c14d9ebb68d729dca14ce6e55fecf3ef90c28
Andrey <andrey@fattoriaweb.com.br>
2016-12-27 18:47:36 -0200

    Continuando o desafioFinal

--- estudosTreinaweb/JS Basico/js/desafioFinal.js
+++ estudosTreinaweb/JS Basico/js/desafioFinal.js
@@ -1,1 +1,19 @@
 // alert('Birl');
+
+var form = document.getElementById('form');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    var senhaDigitada = document.getElementById('senha').value;
+    var textoConversa = document.getElementById('texto');
+    var textoSenha = '<h1>'+ senhaDigitada +'</h1>';
+    textoConversa.innerHTML = textoSenha;
+    console.log(senhaDigitada);
+});
+
+for(i = 0; i <= 20; i++){
+    console.log('tentativa ' + i);
+    if(i == 20){
+        console.log('Acaboooooou');
+    }
+}
