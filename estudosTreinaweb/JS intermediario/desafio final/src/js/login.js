762e627424eb12ad17e5a292f7180244c0ab95e0
Andrey <andrey@fattoriaweb.com.br>
2017-01-11 19:05:52 -0200

    Login quase finzalizado

--- estudosTreinaweb/JS intermediario/desafio final/src/js/login.js
+++ estudosTreinaweb/JS intermediario/desafio final/src/js/login.js
@@ -5,15 +5,16 @@
 
 $(function(){
     var usuariosRegistrados = [
-        { usuario: 'adm', senha: '123456' },
-        { usuario: 'adversario', senha: '852346' },
-        { usuario: 'jose', senha: 'abc156' },
-        { usuario: 'adalberto', senha: '213498' }
-    ]
-    var usuario;
-    var senha;
+            { usuario: 'adm', senha: '123456' },
+            { usuario: 'adversario', senha: '852346' },
+            { usuario: 'jose', senha: 'abc156' },
+            { usuario: 'adalberto', senha: '213498' }
+        ],
+        usuario,
+        senha,
+        logica = false;
 
-    var logica = false;
+    // localStorage.usuariosRegistrados = usuariosRegistrados;
 
     $('#btnEnviar').click(function(e){
         e.preventDefault();
@@ -28,15 +29,71 @@ $(function(){
                 break;
             }
         }
-        if(logica){
-            console.log('Correto');
-        } else{
-            console.log('Usuário ou senha incorretos');
+        try{
+            if(logica){
+                localStorage.usuario = usuario;
+                usuario = $('#usuario').val('');
+                senha = $('#senha').val('');
+                window.location.href = "http://localhost:3000/index.html";
+            } else{
+                throw new Error;
+            }
+        }
+        catch(e){
+            $('.txtErro').html('Usuário ou senha incorretos');
+            $('.txtErro').toggleClass('ativo');
+            setTimeout(function(){
+                $('.txtErro').toggleClass('ativo');
+            }, 3000);
+        }
+    });
+
+    $('#btnCadastrar').click(function(e){
+        e.preventDefault();
+        usuario = $('#usuario').val();
+        senha = $('#senha').val();
+        try{
+            if(usuario && senha){
+                for(i in usuariosRegistrados){
+                    if(usuariosRegistrados[i].usuario == usuario){
+                        logica = true;
+                    }
+                }
+                try{
+                    if(logica){
+                        throw new Error;
+                    } else{
+                        var novoUsuario = {
+                            usuario: usuario,
+                            senha: senha
+                        };
+                        usuariosRegistrados.push(novoUsuario);
+                        usuario = $('#usuario').val('');
+                        senha = $('#senha').val('');
+                    }
+                }catch(e){
+                    $('.txtErro').html('Este usuário já existe.');
+                    $('.txtErro').toggleClass('ativo');
+                    setTimeout(function(){
+                        $('.txtErro').toggleClass('ativo');
+                    }, 3000);
+                }
+            } else{
+                throw new Error;
+            }
+        } catch(e){
+            $('.txtErro').html('Você não pode registrar valores vazios.');
+            $('.txtErro').toggleClass('ativo');
+            setTimeout(function(){
+                $('.txtErro').toggleClass('ativo');
+            }, 3000);
         }
     });
 
     $('#convidado').click(function(e){
         e.preventDefault();
+        localStorage.usuario = 'Convidado';
+        window.location.href = "http://localhost:3000/index.html";
     });
 
 });
--- estudosTreinaweb/JS intermediario/desafio final/src/js/snake.js
+++ estudosTreinaweb/JS intermediario/desafio final/src/js/snake.js
@@ -4,5 +4,11 @@
  */
 
 $(function(){
-
+    var usuario = localStorage.usuario;
+    if(usuario == 'Convidado'){
+        $('.msg').append('<p> Você está conectado como convidado,'+ '<br>' + 'algumas funcionalidades do jogo podem não funcionar corretamente.');
+    } else{
+        $('.msg').append('<p> Bem vindo, ' + usuario);
+    }
+    $('#user').html(usuario);
 });
