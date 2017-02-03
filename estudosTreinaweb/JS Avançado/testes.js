a1249977ac57e4b3c5a32861f4d6b3dfea29d37e
Andrey <andrey@fattoriaweb.com.br>
2017-02-03 19:02:02 -0200

    Ajuste de teste

--- estudosTreinaweb/JS Avançado/testes.js
+++ estudosTreinaweb/JS Avançado/testes.js
@@ -1,19 +1,20 @@
-var sum = function(v1, v2){
-    return v1 + v2;
-}
-
-QUnit.test( "Teste Equal", function( assert ) {
-  assert.equal( 1, "1", "1 e '1' são iguais" );
-  assert.equal(sum(2,3),5);
-  assert.notEqual( 3, 2, "3 e 2 não são iguais" );
-});
+// var sum = function(v1, v2){
+//     return v1 + v2;
+// }
 
 // QUnit.test( "Teste Equal", function( assert ) {
-//   var foo = {name:'TreinaWeb'};
-//   var bar = {name:'TreinaWeb'};
-//   assert.deepEqual( foo, bar, "Objetos iguais" );
-//   assert.notDeepEqual( foo, bar, "Objetos diferentes" );
+//   assert.equal( 1, "1", "1 e '1' são iguais" );
+//   assert.equal(sum(2,3),5);
+//   assert.notEqual( 3, 2, "3 e 2 não são iguais" );
 // });
+
+QUnit.test( "Teste Equal", function( assert ) {
+  var foo = {name:'TreinaWeb'};
+  var bar = {name:'TreinaWeb'};
+  assert.deepEqual( foo, bar, "Objetos iguais" );
+  assert.equal( foo.name, bar.name, "nomes iguais" );
+  assert.notDeepEqual( foo.name, bar.name = "toma", "Opa. são diferentes" );
+});
 
 // QUnit.test( "Teste Expect", function( assert ) {
 //   assert.expect(2);
