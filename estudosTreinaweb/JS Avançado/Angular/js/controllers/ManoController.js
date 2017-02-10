bfc332897ff66e877dec023792f2133da115bb10
Andrey <andrey@fattoriaweb.com.br>
2017-02-10 14:55:33 -0200

    Não terminada aula de angular

--- estudosTreinaweb/JS Avançado/Angular/js/controllers/ManoController.js
+++ estudosTreinaweb/JS Avançado/Angular/js/controllers/ManoController.js
@@ -1,4 +1,18 @@
 SaiaVermelha.controller("ManoController",["$scope","ManoService", function($scope, ManoService){
-    $scope.appName = "Saia Vermelha";
-    ManoService.test();
+    $scope.money = ManoService.money;
+
+    $scope.new = function(){
+        try{
+            if($scope.type === 'incoming'){
+                ManoService.newIncoming({name:$scope.name, value:$scope.value});
+            }else if($scope.type === 'expense'){
+                ManoService.newExpense({name:$scope.name, value:$scope.value});
+            }
+        }catch(e){
+            alert(e.message);
+        }
+    }
+
+    $scope.deleteIncoming = ManoService.deleteIncoming;
+	$scope.deleteExpense = ManoService.deleteExpense;
 }]);
